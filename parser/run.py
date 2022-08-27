f27bac54f2052cd141dabea218703397e9d7a392
Jongsoo Jeong <jsjeong@coxlab.kr>
2022-08-28 01:04:10 +0900

    LS-9 LS-10 Now supported EM310-TILT, and EM310-UDL.

--- parser/run.py
+++ parser/run.py
@@ -5,13 +5,74 @@ import base64
 
 url = None
 
-def post_process(message):
+def post_process_em310_tilt(message):
     if message.get('lora_meta') is None or message['lora_meta'].get('raw') is None:
-        print(f'A message have no lora_meta.raw from Group ID:{message["grpid"]}, Node ID:{message["nid"]}')
+        print(f'[EM310-TILT] A message have no lora_meta.raw from Group ID:{message["grpid"]}, Node ID:{message["nid"]}')
         return message
 
     raw = base64.b64decode(message['lora_meta']['raw'])
-    print(f'Group ID:{message["grpid"]}, Node ID:{message["nid"]}, type:{message["ntype"]}, desc.:{message["ndesc"]}, data:{message["data"]}, raw:{raw}')
+    print(f'[EM310-TILT] Group ID:{message["grpid"]}, Node ID:{message["nid"]}, type:{message["ntype"]}, desc.:{message["ndesc"]}, data:{message["data"]}, raw:{raw}')
+
+    index = 0
+    
+    while index < len(raw):
+        if raw[index + 1] == 0x75:
+            message['data'][f'CH{raw[index]}_Battery_%'] = int.from_bytes(raw[index + 2:index + 3], 'little', signed=False)
+            index += 3
+        elif raw[index + 1] == 0xCF:
+            message['data'][f'CH{raw[index]}_Angle_X_deg'] = int.from_bytes(raw[index + 2:index + 4], 'little', signed=True) * 0.01
+            message['data'][f'CH{raw[index]}_Angle_Y_deg'] = int.from_bytes(raw[index + 4:index + 6], 'little', signed=True) * 0.01
+            message['data'][f'CH{raw[index]}_Angle_Z_deg'] = int.from_bytes(raw[index + 6:index + 8], 'little', signed=True) * 0.01
+            message['data'][f'CH{raw[index]}_Angle_X_flag'] = (raw[index + 8] & 0x01 != 0)
+            message['data'][f'CH{raw[index]}_Angle_Y_flag'] = (raw[index + 8] & 0x02 != 0)
+            message['data'][f'CH{raw[index]}_Angle_Z_flag'] = (raw[index + 8] & 0x04 != 0)
+            index += 9
+        else:
+            print(f"Unknown type 0x{raw[index + 1]:x}")
+            break
+
+    print(message['data'])
+    return message
+
+def post_process_em310_udl(message):
+    if message.get('lora_meta') is None or message['lora_meta'].get('raw') is None:
+        print(f'[EM310-UDL] A message have no lora_meta.raw from Group ID:{message["grpid"]}, Node ID:{message["nid"]}')
+        return message
+
+    raw = base64.b64decode(message['lora_meta']['raw'])
+    print(f'[EM310-UDL] Group ID:{message["grpid"]}, Node ID:{message["nid"]}, type:{message["ntype"]}, desc.:{message["ndesc"]}, data:{message["data"]}, raw:{raw}')
+
+    index = 0
+    
+    while index < len(raw):
+        if raw[index + 1] == 0x00:
+            if raw[index + 2] == 0x00:
+                message['data'][f'CH{raw[index]}_DevicePosition'] = 'normal'
+            elif raw[index + 2] == 0x01:
+                message['data'][f'CH{raw[index]}_DevicePosition'] = 'tilt'
+            else:
+                message['data'][f'CH{raw[index]}_DevicePosition'] = f'unknown({raw[index + 2]})'
+            index += 3
+        elif raw[index + 1] == 0x75:
+            message['data'][f'CH{raw[index]}_Battery_%'] = int.from_bytes(raw[index + 2:index + 3], 'little', signed=False)
+            index += 3
+        elif raw[index + 1] == 0x82:
+            message['data'][f'CH{raw[index]}_Distance_mm'] = int.from_bytes(raw[index + 2:index + 4], 'little', signed=False)
+            index += 4
+        else:
+            print(f"Unknown type 0x{raw[index + 1]:x}")
+            break
+
+    print(message['data'])
+    return message
+
+def post_process_em500(message):
+    if message.get('lora_meta') is None or message['lora_meta'].get('raw') is None:
+        print(f'[EM500]A message have no lora_meta.raw from Group ID:{message["grpid"]}, Node ID:{message["nid"]}')
+        return message
+
+    raw = base64.b64decode(message['lora_meta']['raw'])
+    print(f'[EM500]Group ID:{message["grpid"]}, Node ID:{message["nid"]}, type:{message["ntype"]}, desc.:{message["ndesc"]}, data:{message["data"]}, raw:{raw}')
 
     index = 0
     
@@ -69,6 +130,10 @@ if __name__ == '__main__':
     print(f"URL: {args.url}")
     url = args.url.strip()
 
-    client = pyiotown.post.postprocess_common(args.url, 'Milesight EM500', post_process, args.user.strip(), args.token.strip())
-    pyiotown.post.postprocess_loop_forever(client)
+    clients = []
+    clients.append(pyiotown.post.postprocess_common(args.url, 'Milesight EM310-TILT', post_process_em310_tilt, args.user.strip(), args.token.strip()))
+    clients.append(pyiotown.post.postprocess_common(args.url, 'Milesight EM310-UDL', post_process_em310_udl, args.user.strip(), args.token.strip()))
+    clients.append(pyiotown.post.postprocess_common(args.url, 'Milesight EM500', post_process_em500, args.user.strip(), args.token.strip()))
+    
+    pyiotown.post.postprocess_loop_forever(clients)
     
