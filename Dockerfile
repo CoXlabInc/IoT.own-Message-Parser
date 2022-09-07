6bc0c209a1985eb233347482863f269ac9f82f71
Jongsoo Jeong <jsjeong@coxlab.kr>
2022-09-07 09:49:18 +0900

    Fixed to be able to specify MQTT URL.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM python:3-alpine
 WORKDIR /root/
 
-RUN python3 -m pip install pyiotown==0.4.0
+RUN python3 -m pip install pyiotown==0.4.1
 
 COPY ./parser .
 
-CMD python3 run.py
+CMD [ "python3", "-u", "run.py", "--url", "$IOTOWN_URL", "--user", "$IOTOWN_USER", "--token", "$IOTOWN_TOKEN", "--dry", "$DRY_RUN" ]
--- parser/run.py
+++ parser/run.py
@@ -1,4 +1,6 @@
 import argparse
+from urllib.parse import urlparse
+
 import pyiotown.post
 import pyiotown.post_process
 import pyiotown.get
@@ -16,6 +18,7 @@ if __name__ == '__main__':
     parser.add_argument("--url", help="IoT.own URL", required=True)
     parser.add_argument("--user", help="IoT.own user name", required=True)
     parser.add_argument("--token", help="IoT.own API Token", required=True)
+    parser.add_argument("--mqtt_url", help="MQTT broker URL for IoT.own", required=False, default=None)
     parser.add_argument('--dry', help=" Do not upload data to the server", type=int, default=0)
     args = parser.parse_args()
 
@@ -23,18 +26,26 @@ if __name__ == '__main__':
     print(f"URL: {args.url}")
     url = args.url.strip()
 
+    if args.mqtt_url is not None:
+        mqtt_host = urlparse(args.mqtt_url).hostname
+        mqtt_port = urlparse(args.mqtt_url).port
+        print(f"MQTT URL: tcp://{mqtt_host}:{mqtt_port}")
+    else:
+        mqtt_host = None
+        mqtt_port = 8883
+        
     if args.dry == 1:
         dry_run = True
         print("DRY RUNNING!")
 
     clients = []
-    clients.append(pyiotown.post_process.connect_common(url, 'Milesight AM300', Milesight.am300.post_process, args.user.strip(), args.token.strip(), dry_run=dry_run))
-    clients.append(pyiotown.post_process.connect_common(url, 'Milesight EM300', Milesight.em300.post_process, args.user.strip(), args.token.strip(), dry_run=dry_run))
-    clients.append(pyiotown.post_process.connect_common(url, 'Milesight EM310-TILT', Milesight.em310_tilt.post_process, args.user.strip(), args.token.strip(), dry_run=dry_run))
-    clients.append(pyiotown.post_process.connect_common(url, 'Milesight EM310-UDL', Milesight.em310_udl.post_process, args.user.strip(), args.token.strip(), dry_run=dry_run))
-    clients.append(pyiotown.post_process.connect_common(url, 'Milesight EM500', Milesight.em500.post_process, args.user.strip(), args.token.strip(), dry_run=dry_run))
+    clients.append(pyiotown.post_process.connect_common(url, 'Milesight AM300', Milesight.am300.post_process, args.user.strip(), args.token.strip(), mqtt_host, mqtt_port, dry_run=dry_run))
+    clients.append(pyiotown.post_process.connect_common(url, 'Milesight EM300', Milesight.em300.post_process, args.user.strip(), args.token.strip(), mqtt_host, mqtt_port, dry_run=dry_run))
+    clients.append(pyiotown.post_process.connect_common(url, 'Milesight EM310-TILT', Milesight.em310_tilt.post_process, args.user.strip(), args.token.strip(), mqtt_host, mqtt_port, dry_run=dry_run))
+    clients.append(pyiotown.post_process.connect_common(url, 'Milesight EM310-UDL', Milesight.em310_udl.post_process, args.user.strip(), args.token.strip(), mqtt_host, mqtt_port, dry_run=dry_run))
+    clients.append(pyiotown.post_process.connect_common(url, 'Milesight EM500', Milesight.em500.post_process, args.user.strip(), args.token.strip(), mqtt_host, mqtt_port, dry_run=dry_run))
 
-    clients.append(pyiotown.post_process.connect_common(url, 'Cuetech', Cuetech.post_process, args.user.strip(), args.token.strip(), dry_run=dry_run))
+    clients.append(pyiotown.post_process.connect_common(url, 'Cuetech', Cuetech.post_process, args.user.strip(), args.token.strip(), mqtt_host, mqtt_port, dry_run=dry_run))
     
     pyiotown.post_process.loop_forever(clients)
     
