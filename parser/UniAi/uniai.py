29e1a273ecbf173cfba02ec50e8716254ebe9c21
Jongsoo Jeong <jsjeong@coxlab.kr>
2023-11-01 15:52:23 +0900

    Added length check.

--- parser/UniAi/uniai.py
+++ parser/UniAi/uniai.py
@@ -70,17 +70,26 @@ def post_process(message):
     
   if device['type'] == 'CSD3':
     raw = base64.b64decode(message['meta']['raw'])
-    print(f"[{TAG}] {raw}")
-    if raw[0] == 0x01:
+    if len(raw) == 0:
+      raise Exception("no message")
+
+    sensor_type = raw[0]
+    if sensor_type == 0x01:
       if device['sensor'] == 'T9602':
+        if len(raw) < 5:
+          raise Exception(f"invalid message length ({len(raw)}) for T9602")
         message['data']['Temperature_DegC'] = int.from_bytes(raw[1:3], 'little', signed=True) / 100
         message['data']['Humidity_%'] = int.from_bytes(raw[3:5], 'little', signed=False) / 100
       else:
         raise Exception(f"unsupported temperature sensor '{device['sensor']}'")
-    elif raw[0] == 0x02:
+    elif sensor_type == 0x02:
       if device['sensor'] == 'MH-410D':
+        if len(raw) < 3:
+          raise Exception(f"invalid message length ({len(raw)}) for MH-410D")
         message['data']['CO2_ppm'] = int.from_bytes(raw[1:3], 'big', signed=False)
       elif device['sensor'] == 'ZE03-NH3':
+        if len(raw) < 3:
+          raise Exception(f"invalid message length ({len(raw)}) for ZE03-NH3")
         message['data']['NH3_ppm'] = int.from_bytes(raw[1:3], 'big', signed=False)
       else:
         raise Exception(f"unsupported gas sensor '{device['sensor']}'")
