e7c726f61240ce783b20d8379b11677b6810e489
Jongsoo Jeong <jsjeong@coxlab.kr>
2023-12-11 15:59:11 +0900

    PL-225 Added 2D.

--- parser/CoXlab/trilateration.py
+++ parser/CoXlab/trilateration.py
@@ -4,6 +4,7 @@ import pyiotown.post_process
 import redis
 from urllib.parse import urlparse
 import numpy as np
+import itertools
 
 TAG = 'Trilateration'
 
@@ -74,17 +75,51 @@ def post_process(message, param=None):
     distances = []
     for a in message['data']['anchors'].keys():
         anchor = message['data']['anchors'][a]
-        distances.append([anchor['x'], anchor['y'], anchor['z'], anchor['dist']])
+        if anchor['dist'] >= 0:
+            distances.append([anchor['x'], anchor['y'], anchor['z'], anchor['dist']])
 
-    try:
-        calculated = trilaterate3D(distances)
-        print(f"[{TAG}] {distances} => {calculated}")
-    except Exception as e:
-        print(e)
+    i = 0
+    for d in list(itertools.combinations(distances, 4)):
+        try:
+            calculated = trilaterate3D(d)
+            print(f"[{TAG}] {d} => {calculated}")
+        except Exception as e:
+            print(e)
 
-    message['data']['estimated_x'] = None if np.isnan(calculated[0]) else calculated[0]
-    message['data']['estimated_y'] = None if np.isnan(calculated[1]) else calculated[1]
-    message['data']['estimated_z'] = None if np.isnan(calculated[2]) else calculated[2]
+        message['data'][f'estimated_3d{i}_x'] = None if np.isnan(calculated[0]) else calculated[0]
+        message['data'][f'estimated_3d{i}_y'] = None if np.isnan(calculated[1]) else calculated[1]
+        message['data'][f'estimated_3d{i}_z'] = None if np.isnan(calculated[2]) else calculated[2]
+        i += 1
+
+    i = 0
+
+    x_2d = []
+    y_2d = []
+    
+    for d in list(itertools.combinations(distances, 3)):
+        try:
+            calculated = trilaterate2D(d)
+            print(f"[{TAG}] {d} => {calculated}")
+        except Exception as e:
+            print(e)
+
+        if np.isnan(calculated[0]):
+            message['data'][f'estimated_2d{i}_x'] = None
+        else:
+            message['data'][f'estimated_2d{i}_x'] = calculated[0]
+            x_2d.append(calculated[0])
+
+        if np.isnan(calculated[1]):
+            message['data'][f'estimated_2d{i}_y'] = None
+        else:
+            message['data'][f'estimated_2d{i}_y'] = calculated[1]
+            y_2d.append(calculated[1])
+
+        i += 1
+
+    if len(x_2d) > 0 and len(y_2d) > 0:
+        message['data']['estimated_2d_x'] = np.mean(x_2d)
+        message['data']['estimated_2d_y'] = np.mean(y_2d)
 
     return message
 
@@ -115,3 +150,34 @@ def trilaterate3D(distances):
         return ans1
     else: 
         return ans2
+
+#def trilaterate(anchor_x, anchor_y, anchor1, anchor2, anchor3):
+def trilaterate2D(distances):
+    """
+    @brief: Trilaterate Tag location
+    @param: anchor_x - List of anchor coordinates along the X-axis
+            anchor_y - List of anchor coordinates along the Y-axis
+            anchor1 - Distance to the 1st Anchor
+            anchor2 - Distance to the 2nd Anchor
+            anchor3 - Distance to the 3rd Anchor
+    @ret:   tag_coordinates - Tag Coordinates in a numpy array.
+    """
+    r1_sq = distances[0][-1] ** 2
+    r2_sq = distances[1][-1] ** 2
+    r3_sq = distances[2][-1] ** 2
+
+    # Solve a linear matrix equation where x,y is the Tag coordinate:
+    # Ax + By = C
+    # Dx + Ey = F
+    A = (-2*distances[0][0]) + (2*distances[1][0])
+    B = (-2*distances[0][1]) + (2*distances[1][1])
+    C = r1_sq - r2_sq - (distances[0][0] ** 2) + (distances[1][0] ** 2) - (distances[0][1] ** 2) + (distances[1][1] ** 2) 
+    D = (-2*distances[1][0]) + (2*distances[2][0])
+    E = (-2*distances[1][1]) + (2*distances[2][1])
+    F = r2_sq - r3_sq - (distances[1][0] ** 2) + (distances[2][0] ** 2) - (distances[1][1] ** 2) + (distances[2][1] ** 2) 
+
+    a = np.array([[A, B], [D, E]])
+    b = np.array([C, F])
+    tag_coordinates = np.linalg.solve(a, b)
+    #print("Tag Coordinate:", tag_coordinates)
+    return tag_coordinates.tolist()
